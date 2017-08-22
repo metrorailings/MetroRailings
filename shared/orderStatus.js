01abaf4d34a2ae3e841bcc77e02c538d25684257
kinsho <kinsho@gmail.com>
2017-08-21 22:03:11 -0400

    Making changes to the print order page and order status page

--- shared/orderStatus.js
+++ shared/orderStatus.js
@@ -21,17 +21,17 @@ var ACTIVE_STATUSES =
 		pending: 'Your order is <b>pending</b> your approval! Please check your e-mail and see if you received an' +
 			' e-mail from us that should inform you as to how to approve your order.',
 		queue: 'Your order is <b>queued</b> in our backlog. We should begin working on your order within a week' +
-			' after we receive your deposit for the first half.',
-		production: 'Your order is now in <b>production</b> meaning that we are hard at work cutting, shaping, and' +
-			' molding metal in order to build out your railings.',
-		finishing: 'Your order is now in the <b>finishing</b> stage, meaning we are applying powder coating to your' +
+			' after we have received your initial down deposit.',
+		production: 'Your order is now in <b>production</b>, meaning that we are hard at work cutting, shaping, and' +
+			' molding metal in order to construct your railings.',
+		finishing: 'Your order is now in the <b>finishing</b> stages, meaning we are applying powder coating to your' +
 			' railings. Once the railings are treated with the powder coating, we cure the railings under intense' +
-			' heat inside our specially built oven so that the finish truly sticks for years to come.',
+			' heat inside our custom-built oven so that the finish truly sticks for years to come.',
 		install: 'Your order is ready to be <b>installed</b>. Expect a call from us soon to schedule a date and time for us to ' +
 			'come over and install these railings. If we already spoke to you, we will be visiting you soon to install ' +
 			'your new railings.',
-		closed: 'Your order is <b>complete</b>. Your new railings have been built, powder coated, and installed at' +
-			' the address you provided us. We thank you for your business.',
+		closed: 'Your order is <b>complete</b>. Your new railings have been built, powder coated, and installed' +
+		' successfully! We thank you for your business.',
 		cancelled: 'Your order has been <b>cancelled</b>, either because you requested that we cancel the order or because ' +
 			'we were unable to reach out to you at the contact information you gave us.'
 	},
