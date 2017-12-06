eb11ec30f05ac057aff5fed3c407c9a403e0770a
kinsho <kinsho@gmail.com>
2017-12-06 16:51:17 -0500

    Making another big update to further optimize the way the server delivers assets to the client

--- prod/buildScript.js
+++ prod/buildScript.js
@@ -9,18 +9,28 @@ global.OwlStakes =
 
 var	_fs = require('fs'),
 	_Q = require('q'),
+	_cheerio = require('cheerio'),
+	_request = require('request'),
+	_cleanCSS = require('clean-css'),
 
 	fileManager = global.OwlStakes.require('utility/fileManager');
 
 // ----------------- ENUMS/CONSTANTS --------------------------
 
 var	SCRIPTS_DIRECTORY = 'client/scripts/',
+	VIEWS_DIRECTORY = '/client/views/',
 
 	JSPM_PACKAGES_DIRECTORY_KEYWORD = '/jspm_packages/',
 	JSPM_BUNDLE_KEYWORD = 'jspm bundle ',
 	BUNDLE_APPEND_KEYWORD = ' + ',
+	BASE_HANDLEBARS_FILE = 'base.handlebars',
+
+	THIRD_PARTY_SCRIPTS_SECTION = 'thirdPartyScripts',
+	THIRD_PARTY_STYLES_SECTION = 'thirdPartyStyles',
 
 	MAIN_SCRIPT = '/main.js',
+	BUILD_THIRD_PARTY_SCRIPTS_FILE = 'prod/thirdParty.js',
+	BUILD_THIRD_PARTY_STYLESHEETS_FILE = 'prod/thirdParty.css',
 	BUILD_OUTPUT_FILE = 'prod/build.src.js',
 	BUILD_SHELL_FILE = 'prod/buildScript.sh';
 
@@ -33,9 +43,72 @@ var fsWriteFile = _Q.denodeify(_fs.writeFile);
 (async function()
 {
 	var scriptFiles = await fileManager.fetchAllFilePaths(SCRIPTS_DIRECTORY),
+		baseHTML = await fileManager.fetchFile(VIEWS_DIRECTORY + BASE_HANDLEBARS_FILE),
+		thirdPartyScriptURLs, thirdPartyScripts, thirdPartyScriptContents = '',
+		thirdPartyStylesheetURLs, thirdPartyStylesheets, thirdPartyStylesheetContents = '',
 		scriptFile, buildString,
 		filesToBuild = [],
+		cssCleaner = new _cleanCSS({ level : 1 }),
 		i;
+
+	// Process the HTML so that we can better navigate to fetch the third-party scripts we need to pull
+	baseHTML = _cheerio.load(baseHTML);
+	thirdPartyScriptURLs = baseHTML('#' + THIRD_PARTY_SCRIPTS_SECTION).find('script');
+	thirdPartyStylesheetURLs = baseHTML('#' + THIRD_PARTY_STYLES_SECTION).find('link');
+
+	// Fetch all third-party scripts in raw form
+	thirdPartyScripts = await _Q.all(thirdPartyScriptURLs.map((index, data) =>
+	{
+		var deferred = _Q.defer();
+
+		console.log('Fetching ' + data.attribs.src);
+		_request(data.attribs.src, (error, response, body) =>
+		{
+			if (error)
+			{
+				deferred.reject(error);
+			}
+			deferred.resolve(body);
+		});
+
+		return deferred.promise;
+	}));
+
+	// Fetch all third-party stylesheets in raw form
+	thirdPartyStylesheets = await _Q.all(thirdPartyStylesheetURLs.map((index, data) =>
+	{
+		var deferred = _Q.defer();
+
+		console.log('Fetching ' + data.attribs.href);
+		_request(data.attribs.href, (error, response, body) =>
+		{
+			if (error)
+			{
+				deferred.reject(error);
+			}
+			deferred.resolve(body);
+		});
+
+		return deferred.promise;
+	}));
+
+	// Collect the contents of all third-party scripts and print them to a bundle file
+	console.log('Condensing scripts...');
+	for (i = 0; i < thirdPartyScripts.length; i++)
+	{
+		thirdPartyScriptContents += thirdPartyScripts[i];
+	}
+	console.log('Writing out to ' + BUILD_THIRD_PARTY_SCRIPTS_FILE);
+	await fileManager.writeFile(thirdPartyScriptContents, BUILD_THIRD_PARTY_SCRIPTS_FILE);
+
+	// Collect the contents of all third-party stylesheets and print them to a bundle file
+	console.log('Condensing stylesheets...');
+	for (i = 0; i < thirdPartyStylesheets.length; i++)
+	{
+		thirdPartyStylesheetContents += cssCleaner.minify(thirdPartyStylesheets[i]).styles.toString();
+	}
+	console.log('Writing out to ' + BUILD_THIRD_PARTY_STYLESHEETS_FILE);
+	await fileManager.writeFile(thirdPartyStylesheetContents, BUILD_THIRD_PARTY_STYLESHEETS_FILE);
 
 	// Fetch the paths to all main script files
 	for (i = 0; i < scriptFiles.length; i++)
