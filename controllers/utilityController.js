d16b6cd687b12c5c82a092b65cc4694a3e10368e
kinsho <kinsho@gmail.com>
2018-02-19 19:07:25 -0500

    Began work on moving the actual translation work to the server-side

---
+++ controllers/utilityController.js
@@ -0,0 +1,18 @@
+/**
+ * @module utilityController
+ */
+
+// ----------------- MODULE DEFINITION --------------------------
+
+module.exports =
+{
+	/**
+	 * Function responsible for translating text
+	 *
+	 * @author kinsho
+	 */
+	translateText: async function(params)
+	{
+		console.log('Translating text...');
+	}
+};
