beea1e724d3eaec7dfec63edd20d8b5291230761
kinsho <kinsho@gmail.com>
2018-02-20 16:14:43 -0500

    Made a few changes to the production build process in order to make the whole process more robust

--- prod/minify.sh
+++ prod/minify.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Beginning minification..."
+echo "Minifying scripts..."
 uglifyjs prod/build.src.js > prod/app.js
+echo "Minifying stylesheets..."
 node prod/condenseStylesheets.js
 echo "Minification done!"
