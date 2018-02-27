f43fc834033f0753f63a0be568d2b77101043db2
kinsho <kinsho@gmail.com>
2018-02-27 14:07:13 -0500

    Almost done with work on translation for the most important admin pages

--- client/scripts/orders/orderListing.js
+++ client/scripts/orders/orderListing.js
@@ -10,6 +10,7 @@ import notifier from 'client/scripts/utility/notifications';
 import confirmationModal from 'client/scripts/utility/confirmationModal';
 import rQueryClient from 'client/scripts/utility/rQueryClient';
 import gallery from 'client/scripts/utility/gallery';
+import translator from 'client/scripts/utility/translate';
 
 // ----------------- ENUMS/CONSTANTS ---------------------------
 
@@ -33,9 +34,11 @@ var ORDER_PICTURES_TEMPLATE = 'orderPicturesTemplate',
 	UPLOADED_IMAGE_THUMBNAIL_CLASS = 'uploadedImageThumbnail',
 	HIDE_CLASS = 'hide',
 
-	NEXT_STATUS_MESSAGE = '<span>Are you sure you want to update order <b>::orderID</b> to <b>::nextStatus</b>?</span>',
-	DELETE_ORDER_MESSAGE = '<span>Are you sure you want to <b>DELETE</b> this potential order? Once you delete this' +
-		' order, you will need to go to the database administrator to restore this prospect back into the system.</span>',
+	NEXT_STATUS_MESSAGE = '<span> Are you sure you want to update order <b> ::orderID </b> to' +
+		' <b> ::nextStatus </b> ?</span>',
+	DELETE_ORDER_MESSAGE = '<span> Are you sure you want to <b> DELETE </b> this potential order? Once you delete' +
+		' this order, you will need to go to the database administrator to restore this prospect back into the' +
+		' system. </span>',
 
 	UPDATE_STATUS_URL = 'orders/updateStatus',
 	DELETE_ORDER_URL = 'orders/removeOrder',
@@ -304,7 +307,7 @@ function openGallery(event)
  *
  * @author kinsho
  */
-function updateStatus(event)
+async function updateStatus(event)
 {
 	var targetElement = event.currentTarget,
 		orderID = window.parseInt(targetElement.dataset.id, 10),
@@ -315,26 +318,30 @@ function updateStatus(event)
 		modalMessage = NEXT_STATUS_MESSAGE.replace(ORDER_ID_PLACEHOLDER, orderID)
 			.replace(NEXT_STATUS_PLACEHOLDER, rQueryClient.capitalize(nextStatus));
 
-	// Confirm that we will be updating the status
-	confirmationModal.open([modalMessage], () =>
+	// Translate the modal's message should the page's default language be set in anything other than English
+	translator.translateText(modalMessage).then((modalBodyText) =>
 	{
-		// Update the status in the back-end, then use the results from the service to modify the local copy of data as needed
-		axios.post(UPDATE_STATUS_URL, { orderID: orderID }).then((results) =>
+		// Confirm that we will be updating the status
+		confirmationModal.open([modalBodyText], () =>
 		{
-			// Update the last known modification date for the order in context
-			order.lastModifiedDate = results.data.lastModifiedDate;
+			// Update the status in the back-end, then use the results from the service to modify the local copy of data as needed
+			axios.post(UPDATE_STATUS_URL, { orderID: orderID }).then((results) =>
+			{
+				// Update the last known modification date for the order in context
+				order.lastModifiedDate = results.data.lastModifiedDate;
 
-			// Update the status to the next one in the development cycle
-			order.status = results.data.status;
+				// Update the status to the next one in the development cycle
+				order.status = results.data.status;
 
-			// Move the newly modified order to the front of the pack to indicate that it was the order most recently modified
-			vm.orders.splice(orderIndex, 1);
-			vm.orders.unshift(order);
-		}, () =>
-		{
-			notifier.showGenericServerError();
-		});
+				// Move the newly modified order to the front of the pack to indicate that it was the order most recently modified
+				vm.orders.splice(orderIndex, 1);
+				vm.orders.unshift(order);
+			}, () =>
+			{
+				notifier.showGenericServerError();
+			});
 
+		}, () => {});
 	}, () => {});
 }
 
@@ -353,23 +360,27 @@ function removeOrder(event)
 		modalMessage = DELETE_ORDER_MESSAGE,
 		orderBlock = document.getElementById(ORDER_BLOCK_PREFIX + orderID);
 
-	// Confirm that we will be updating the status
-	confirmationModal.open([modalMessage], () =>
+	// Translate the modal's message should the page's default language be set in anything other than English
+	translator.translateText(modalMessage).then((modalBodyText) =>
 	{
-		// Remove the order in the back-end so that it will no longer appear in the front-facing admin system
-		axios.post(DELETE_ORDER_URL, { orderID: orderID }).then(() =>
+		// Confirm that we will be updating the status
+		confirmationModal.open([modalBodyText], () =>
 		{
-			// Remove the order from the cached collection of orders
-			vm.orders.splice(orderIndex, 1);
+			// Remove the order in the back-end so that it will no longer appear in the front-facing admin system
+			axios.post(DELETE_ORDER_URL, { orderID: orderID }).then(() =>
+			{
+				// Remove the order from the cached collection of orders
+				vm.orders.splice(orderIndex, 1);
 
-			// Hide the block associated with this order from view
-			orderBlock.classList.add(HIDE_CLASS);
+				// Hide the block associated with this order from view
+				orderBlock.classList.add(HIDE_CLASS);
 
-		}, () =>
-		{
-			notifier.showGenericServerError();
-		});
+			}, () =>
+			{
+				notifier.showGenericServerError();
+			});
 
+		}, () => {});
 	}, () => {});
 }
 
--- client/scripts/utility/translate.js
+++ client/scripts/utility/translate.js
@@ -4,6 +4,8 @@
 
 // ----------------- EXTERNAL MODULES --------------------------
 
+import _promise from 'es6-promise';
+
 import axios from 'client/scripts/utility/axios';
 import notifier from 'client/scripts/utility/notifications';
 
@@ -17,11 +19,14 @@ var LOADING_VEIL = 'baseLoaderOverlay',
 	TRANSLATE_TO_ENGLISH_LINK = 'englishLanguageTranslator',
 	TRANSLATE_TO_SPANISH_LINK = 'spanishLanguageTranslator',
 
-	ENGLISH_LANGUAGE_DESIGNATION = 'en',
-	SPANISH_LANGUAGE_DESIGNATION = 'es',
-
 	TRANSLATE_TO_ENGLISH_URL = 'utility/translateTextToEnglish',
-	TRANSLATE_TO_SPANISH_URL = 'utility/translateTextToSpanish';
+	TRANSLATE_TO_SPANISH_URL = 'utility/translateTextToSpanish',
+
+	DEFAULT_LANGUAGE_KEY = 'defaultLanguage';
+
+// ----------------- PRIVATE MEMBERS --------------------------
+
+var _defaultLanguage;
 
 // ----------------- ELEMENTS --------------------------
 
@@ -29,44 +34,66 @@ var _veil = document.getElementById(LOADING_VEIL),
 	_englishLink = document.getElementById(TRANSLATE_TO_ENGLISH_LINK),
 	_spanishLink = document.getElementById(TRANSLATE_TO_SPANISH_LINK);
 
-// ----------------- LISTENERS --------------------------
+// ----------------- PRIVATE FUNCTIONS --------------------------
 
 /**
  * Function responsible for translating marked text to whatever language is indicated by the parameter
  *
- * @param {String} languageCode - the language to which to translate marked text
+ * @param {String} [languageCode] - the language to which to translate marked text
+ * @param {Boolean} [forceTranslate] - a flag indicating whether we need to forcefully translate the page's text,
+ * 		regardless of any default language specification
+ * @param {HTMLElement} [rootNode] - if specified, only the DOM elements that fall under this rootNode will have its
+ * 		text translated
  *
  * @author kinsho
  */
-function translatePage(languageCode)
+function _translatePage(languageCode, forceTranslate, rootNode)
 {
-	var translationPoints = document.getElementsByClassName(TO_TRANSLATE_CLASS),
-		textToTranslate = '',
+	let translationPoints,
+		textToTranslate = [],
 		translateURL,
 		i;
 
-	// Figure out which URL to invoke in order to translate the text
-	if (languageCode === ENGLISH_LANGUAGE_DESIGNATION)
+	// Fetch all the marked elements with text that will need to be translated
+	rootNode = rootNode || document;
+	translationPoints = rootNode.querySelectorAll('.' + TO_TRANSLATE_CLASS);
+
+	// If the text on the page is already represented in the language being switched to, then just skip out of this
+	// function, unless the forceTranslate flag is set
+	if ( !(forceTranslate) && (languageCode === window.localStorage.getItem(DEFAULT_LANGUAGE_KEY)) )
 	{
+		return;
+	}
+
+	// Figure out which URL to invoke in order to translate the text. Also update the page to indicate in which
+	// language the page is shown in. And lastly, update the browser cache to indicate in which language the user
+	// prefers to view the page in
+	if (languageCode === translateModule.ENGLISH_LANGUAGE_DESIGNATION)
+	{
+		_englishLink.classList.add(SELECTED_CLASS);
+		_spanishLink.classList.remove(SELECTED_CLASS);
+
+		window.localStorage.setItem(DEFAULT_LANGUAGE_KEY, translateModule.ENGLISH_LANGUAGE_DESIGNATION);
+
 		translateURL = TRANSLATE_TO_ENGLISH_URL;
 	}
-	else if (languageCode === SPANISH_LANGUAGE_DESIGNATION)
+	else if (languageCode === translateModule.SPANISH_LANGUAGE_DESIGNATION)
 	{
+		_englishLink.classList.remove(SELECTED_CLASS);
+		_spanishLink.classList.add(SELECTED_CLASS);
+
+		window.localStorage.setItem(DEFAULT_LANGUAGE_KEY, translateModule.SPANISH_LANGUAGE_DESIGNATION);
+
 		translateURL = TRANSLATE_TO_SPANISH_URL;
 	}
 
 	// Gather all the text on the page that's designated for translation
 	for (i = 0; i < translationPoints.length; i++)
 	{
-		textToTranslate += translationPoints[i].innerText;
-
-		if (i !== translationPoints.length - 1)
-		{
-			textToTranslate += '|';
-		}
+		textToTranslate.push(translationPoints[i].innerText);
 	}
 
-	if (textToTranslate)
+	if (textToTranslate.length)
 	{
 		// Set up the loading veil to block the page as we translate the text
 		_veil.classList.add(VISIBILITY_CLASS);
@@ -75,7 +102,8 @@ function translatePage(languageCode)
 		axios.post(translateURL, { textToTranslate: textToTranslate }).then((results) =>
 		{
 			// Now replace the marked text with their corresponding translated versions
-			results = results.data.split('|');
+			results = results.data;
+
 			for (i = 0; i < translationPoints.length; i++)
 			{
 				translationPoints[i].innerText = results[i];
@@ -91,20 +119,98 @@ function translatePage(languageCode)
 	}
 }
 
+// ----------------- MODULE DEFINITION --------------------------
+
+var translateModule =
+{
+	ENGLISH_LANGUAGE_DESIGNATION: 'en',
+	SPANISH_LANGUAGE_DESIGNATION: 'es',
+
+	/**
+	 * Function translates a given piece of text or a collection of strings if the default language is anything
+	 * other than English
+	 *
+	 * @param {String | Array<String>} text - either a single piece of text or a collection of strings that need to
+	 * 		be translated
+	 * @param {String} languageCode - the language to which to translate marked text
+	 *
+	 * @returns {String | Array<String>} - the string(s), possibly translated, returned back in whatever form their
+	 * 		original variants were initially passed into the function
+	 *
+	 * @author kinsho
+	 */
+	translateText: function(text)
+	{
+		return new Promise((resolve, reject) =>
+		{
+			if (window.localStorage.getItem(DEFAULT_LANGUAGE_KEY) !== translateModule.ENGLISH_LANGUAGE_DESIGNATION)
+			{
+				// For the time being, we only need to translate the text into Spanish
+				axios.post(TRANSLATE_TO_SPANISH_URL, { textToTranslate: text }, true).then((results) =>
+				{
+					resolve(results.data);
+				}, () =>
+				{
+					_veil.classList.remove(VISIBILITY_CLASS);
+					notifier.showGenericServerError();
+
+					reject('');
+				});
+			}
+			else
+			{
+				resolve(text);
+			}
+		});
+	},
+
+	/**
+	 * Function translates a section of page forcefully if the default language is anything other than English
+	 *
+	 * @param {HTMLElement} [rootNode] - if specified, only the DOM elements that fall under this rootNode will have its
+	 * 		text translated
+	 *
+	 * @author kinsho
+	 */
+	forceTranslatePage: function(rootNode)
+	{
+		rootNode = rootNode || document;
+
+		if (window.localStorage.getItem(DEFAULT_LANGUAGE_KEY) !== translateModule.ENGLISH_LANGUAGE_DESIGNATION)
+		{
+			_translatePage(window.localStorage.getItem(DEFAULT_LANGUAGE_KEY), true, rootNode);
+		}
+	}
+};
+
 // ----------------- INITIALIZATION --------------------------
 
 // Attach event listeners to the language links
 _englishLink.addEventListener('click', function()
 {
-	_englishLink.classList.add(SELECTED_CLASS);
-	_spanishLink.classList.remove(SELECTED_CLASS);
-
-	translatePage(ENGLISH_LANGUAGE_DESIGNATION);
+	_translatePage(translateModule.ENGLISH_LANGUAGE_DESIGNATION);
 });
 _spanishLink.addEventListener('click', function()
 {
-	_englishLink.classList.remove(SELECTED_CLASS);
-	_spanishLink.classList.add(SELECTED_CLASS);
-
-	translatePage(SPANISH_LANGUAGE_DESIGNATION);
+	_translatePage(translateModule.SPANISH_LANGUAGE_DESIGNATION);
 });
+
+// Track whatever the default language is for the user navigating the page
+_defaultLanguage = window.localStorage.getItem(DEFAULT_LANGUAGE_KEY);
+
+// If no default language is set, set the English language as the de facto default
+if ( !(_defaultLanguage) )
+{
+	window.localStorage.setItem(DEFAULT_LANGUAGE_KEY, translateModule.ENGLISH_LANGUAGE_DESIGNATION);
+	_defaultLanguage = translateModule.ENGLISH_LANGUAGE_DESIGNATION;
+}
+
+// If the default language is something other than English, translate the page into that default language
+if (_defaultLanguage !== translateModule.ENGLISH_LANGUAGE_DESIGNATION)
+{
+	_translatePage(_defaultLanguage, true);
+}
+
+// ----------------- EXPORT -----------------------------
+
+export default translateModule;
