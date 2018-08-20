ad99d00b0135e688dd7da8ac5eaafdb68ea7f3e9
kinsho <kinsho@gmail.com>
2018-08-20 11:07:03 -0400

    Still working on redesigning the way design options are selected for any order

--- shared/designs/postCapDesigns.js
+++ shared/designs/postCapDesigns.js
@@ -5,7 +5,8 @@
  */
 // ----------------- EXTERNAL MODULES --------------------------
 
-import pricing from 'shared/pricing/pricingData';
+// @TODO - Incorporate pricing here
+// import pricing from 'shared/pricing/pricingData';
 
 // ----------------- MODULE DEFINITION --------------------------
 
@@ -14,47 +15,39 @@ var postCapDesigns =
 	options:
 	[
 		{
-			id: 'PC-BALL',
-			label: 'Ball',
-			previewImage: 'client/images/designs/postCap/ball-1.jpg'
+			id: 'PC-NONE',
+			label: 'N/A',
+			technicalLabel: 'N/A',
+			spanishLabel: 'N/A'
 		},
 		{
-			id: 'PC-SQ',
-			label: 'Square',
-			previewImage: 'client/images/designs/postCap/square-1.jpg',
+			id: 'PC-BALL',
+			label: 'Ball',
+			technicalLabel: 'Ball',
+			spanishLabel: 'Pelota'
+		},
+		{
+			id: 'PC-FLAT',
+			label: 'Flat',
+			technicalLabel: 'Flat',
+			spanishLabel: 'Plano'
+		},
+		{
+			id: 'PC-PYR',
+			label: 'Pyramid',
+			technicalLabel: 'Pyramid',
+			spanishLabel: 'Pirámide'
+		},
+		{
+			id: 'PC-RGL',
+			label: 'Regal',
+			technicalLabel: 'Regal',
+			spanishLabel: 'Regio'
 		}
 	],
-	designMetadata:
-	[
-		{
-			id: 'PC-BALL',
-			label: 'Ball',
-			price: pricing.DESIGNS['PC-BALL'].price,
-			designDescription: 'The standard-bearer in post caps. Fixing these orbs on top of each post will ensure ' +
-				'your railings stand out with a solemn grace.',
-			designImages:
-			[
-				'client/images/designs/postCap/ball-2.jpg',
-				'client/images/designs/postCap/ball-3.jpg',
-				'client/images/designs/postCap/ball-4.jpg'
-			],
-		},
-		{
-			id: 'PC-SQ',
-			label: 'Square',
-			price: pricing.DESIGNS['PC-SQ'].price,
-			designDescription: 'Simple and beautiful, these flat-top caps work best for those that want their posts to ' +
-				'blend in effortlessly with the rest of their railings.',
-			designImages:
-			[
-				'client/images/designs/postCap/square-2.jpg',
-				'client/images/designs/postCap/square-3.jpg',
-				'client/images/designs/postCap/square-4.jpg'
-			]
-		}
-	]
+	designMetadata: []
 };
 
 // ----------------- EXPORT --------------------------
 
-export default postCapDesigns;
+module.exports = postCapDesigns;
