fc326119ec462493956cb360fe7606e6b47120e7
kinsho <kinsho@gmail.com>
2018-08-21 21:27:38 -0400

    Now working on an overhaul of the create quote page, given that the additional design options that are now being included mandate changes in the business logic

--- client/scripts/createQuote/main.js
+++ client/scripts/createQuote/main.js
@@ -2,7 +2,9 @@
 
 import customerSection from 'client/scripts/createQuote/customerSection';
 import addressSection from 'client/scripts/createQuote/addressSection';
-import railingsSection from 'client/scripts/createQuote/railingsSection';
+import baseDesignSection from 'client/scripts/createQuote/baseDesignSection';
+import advancedDesignSection from 'client/scripts/createQuote/advancedDesignSection';
+import logisticsSection from 'client/scripts/createQuote/logisticsSection';
 import externalCharges from 'client/scripts/createQuote/externalCharges';
 import agreementSection from 'client/scripts/createQuote/agreementSection';
 import submissionSection from 'client/scripts/createQuote/submissionSection';
