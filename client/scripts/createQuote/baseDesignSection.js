e26221d8d27ecd85c968c55e20516226fe933b3c
kinsho <kinsho@gmail.com>
2018-08-21 21:28:16 -0400

    Adding some files with the last commit

---
+++ client/scripts/createQuote/baseDesignSection.js
@@ -0,0 +1,161 @@
+// ----------------- EXTERNAL MODULES --------------------------
+
+import vm from 'client/scripts/createQuote/viewModel';
+
+// ----------------- ENUMS/CONSTANTS ---------------------------
+
+var TYPE_SELECT = 'orderType',
+	POST_DESIGN_SELECT = 'orderPost',
+	HANDRAILING_SELECT = 'orderHandrailing',
+	PICKET_SIZE_SELECT = 'orderPicketSize',
+	POST_END_SELECT = 'orderPostEnd',
+	POST_CAP_SELECT = 'orderPostCap',
+	COLOR_SELECT = 'orderColor',
+
+	OTHER_COLOR_TEXTFIELD = 'otherColor',
+
+	OTHER_VALUE_FIELD_CLASS = 'otherTextfield',
+	SHOW_CLASS = 'show',
+
+	OTHER_SELECTION = 'OTHER';
+
+// ----------------- PRIVATE VARIABLES ---------------------------
+
+var _orderTypeField = document.getElementById(TYPE_SELECT),
+	_orderPostDesignField = document.getElementById(POST_DESIGN_SELECT),
+	_orderHandrailingField = document.getElementById(HANDRAILING_SELECT),
+	_orderPicketField = document.getElementById(PICKET_SIZE_SELECT),
+	_orderPostEndField = document.getElementById(POST_END_SELECT),
+	_orderPostCapField = document.getElementById(POST_CAP_SELECT),
+	_orderColorField = document.getElementById(COLOR_SELECT),
+
+	_otherColorField = document.getElementById(OTHER_COLOR_TEXTFIELD);
+
+// ----------------- PRIVATE METHODS ---------------------------
+
+/**
+ * Function meant to show the 'Other' field to allow the user to put in unlisted values for any design option
+ *
+ * @param {DOMElement} element - the element which to examine to determine if its companion 'Other' textfield should
+ * 		be shown or hidden
+ *
+ * @author kinsho
+ */
+function _toggleOtherFieldVisibility(element)
+{
+	var otherField = element.nextSibling;
+
+	// Only consider toggling the visibility of the companion 'Other' textfield if we are dealing with the select
+	// dropdown here
+	if ( !(element.classList.contains(OTHER_VALUE_FIELD_CLASS)) )
+	{
+		if (element.value === OTHER_SELECTION)
+		{
+			otherField.classList.add(SHOW_CLASS);
+		}
+		else
+		{
+			otherField.classList.remove(SHOW_CLASS);
+		}
+	}
+}
+
+// ----------------- LISTENERS ---------------------------
+
+/**
+ * Listener responsible for setting the order type into the view model
+ *
+ * @author kinsho
+ */
+function setType()
+{
+	vm.design.type = _orderTypeField.value;
+}
+
+/**
+ * Listener responsible for setting the post design into the view model
+ *
+ * @author kinsho
+ */
+function setPostDesign()
+{
+	vm.design.post = _orderPostDesignField;
+}
+
+/**
+ * Listener responsible for setting the handrailing type into the view model
+ *
+ * @author kinsho
+ */
+function setHandrailing()
+{
+	vm.design.handrailing = _orderHandrailingField.value;
+}
+
+/**
+ * Listener responsible for setting the picket size into the view model
+ *
+ * @author kinsho
+ */
+function setPicketSize()
+{
+	vm.design.picket = _orderPicketField.value;
+}
+
+/**
+ * Listener responsible for setting the post end design into the view model
+ *
+ * @author kinsho
+ */
+function setPostEnd()
+{
+	vm.design.postEnd = _orderPostEndField.value;
+}
+
+/**
+ * Listener responsible for setting the post cap design into the view model
+ *
+ * @author kinsho
+ */
+function setPostCap()
+{
+	vm.design.postCap = _orderPostCapField.value;
+}
+
+/**
+ * Listener responsible for setting the order color into the view model
+ *
+ * @author kinsho
+ */
+function setColor(event)
+{
+	var element = event.currentTarget,
+		isOtherSelected = (element.value === OTHER_SELECTION);
+
+	vm.design.color = (isOtherSelected ? _otherColorField.value : element.value);
+
+	_toggleOtherFieldVisibility(_orderColorField);
+}
+
+// ----------------- LISTENER INITIALIZATION -----------------------------
+
+_orderTypeField.addEventListener('change', setType);
+_orderPostDesignField.addEventListener('change', setPostDesign);
+_orderHandrailingField.addEventListener('change', setHandrailing);
+_orderPicketField.addEventListener('change', setPicketSize);
+_orderPostEndField.addEventListener('change', setPostEnd);
+_orderPostCapField.addEventListener('change', setPostCap);
+_orderColorField.addEventListener('change', setColor);
+
+_otherColorField.addEventListener('change', setColor);
+
+// ----------------- DATA INITIALIZATION -----------------------------
+
+setType({ currentTarget: _orderTypeField });
+setPostDesign({ currentTarget: _orderPostDesignField });
+setHandrailing({ currentTarget: _orderHandrailingField });
+setPicketSize({ currentTarget: _orderPicketField });
+setPostEnd({ currentTarget: _orderPostEndField });
+setPostCap({ currentTarget: _orderPostCapField });
+
+setColor({ currentTarget: _orderColorField });
---
+++ shared/designs/picketStyles.js
@@ -0,0 +1,42 @@
+/**
+ * A module listing important information about all the picket styles
+ *
+ * @module picketStyles
+ */
+
+// ----------------- EXTERNAL MODULES --------------------------
+
+// @TODO - Incorporate pricing here
+// import pricing from 'shared/pricing/pricingData';
+
+// ----------------- MODULE DEFINITION --------------------------
+
+var picketStyles =
+{
+	options:
+	[
+		{
+			id: 'PCKT-STY-PLAIN',
+			label: 'Plain Pickets',
+			technicalLabel: 'Plain',
+			spanishLabel: 'Simple'
+		},
+		{
+			id: 'PCKT-STY-TWST',
+			label: 'Twisted Pickets',
+			technicalLabel: 'Twisted',
+			spanishLabel: 'Retorcido'
+		},
+		{
+			id: 'PCKT-STY-BOW',
+			label: 'Bowed Pickets',
+			technicalLabel: 'Bowed',
+			spanishLabel: 'Arco'
+		}
+	],
+	designMetadata: []
+};
+
+// ----------------- EXPORT --------------------------
+
+module.exports = picketStyles;
