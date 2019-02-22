a224ca5fb2e6b78dc14045208e090894ea8bbb4a
kinsho <kinsho@gmail.com>
2019-02-22 16:27:55 -0500

    Committing code prior to my computer getting opened up

---
+++ client/scripts/utility/notes.js
@@ -0,0 +1,31 @@
+/**
+ * @main notes
+ */
+
+// ----------------- ENUMS/CONSTANTS --------------------------
+
+var NOTE_TYPE_SELECT = 'noteType',
+	TASK_ASSIGN_TO_SELECT = 'takeAssignTo',
+	NOTE_SAVE_BUTTON = 'noteSave';
+
+// ----------------- PRIVATE MEMBERS --------------------------
+
+// ----------------- PRIVATE FUNCTIONS --------------------------
+
+// ----------------- LISTENERS --------------------------
+
+// ----------------- MODULE ---------------------------
+
+/**
+ * Constructor function that'll help us manage new notes and tasks as they are generated through the notes interface
+ *
+ * @param {HTMLElement} notesContainer - the HTML container containing the notes section
+ *
+ * @author kinsho
+ */
+function initNotesTextfield(notesContainer)
+{}
+
+// ----------------- EXPORT ---------------------------
+
+export default initNotesTextfield;
